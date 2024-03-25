9b0495548542aa1c470a1ca62c5ecb986a4b8954
Ambiorix Martinez <ambiorix565@gmail.com>
2024-03-25 00:23:41 -0400

    Agregado de tercera preentrega

--- js/main.js
+++ js/main.js
@@ -1,266 +1,118 @@
-//Preentrega-2
+//Preentrega 3
 
-//mensaje de bienvenida
-alert("Bienvenido a la mejor tienda de instrumentos musicales!! Aquí encontrarás: \n\nBaterías acústicas, Baterías eléctricas, platillos y Accesorios. \n\nTodos al mejor precio del mercado. \n\n ¡¡Tenemos interesantes DESCUENTOS para aquellos que elijan más de 3 productos, así que...!! \n\n!Ven y descubrelo!");
+const btnSearch = document.querySelector("#btnSearch"),
+  cardInstrumento = document.querySelector("#card-instrument-tienda"),
+  carritoContenedor = document.getElementById("carritoContenedor"),
+  totalCarritoElement = document.getElementById("totalCarrito");
 
-//Inicio sesion
-let usuario = prompt("Ingrese su nombre")
+const instrumentos = [
+    {id:1, nombre: "Yamaha RDP0F5 HOR Batería acústica Rydeen, Hot Red", precio: 37252.12, descripcion: "Juego de batería de 5 tambores. Color rojo candente" ,img: "bateriaAcustica1.jpg"  },
+    {id:2, nombre: "RDP2F5 PB Batería acústica Rydeen, Plateado brillante Bombo 22", precio: 39524.41, descripcion: "Juego de batería de 5 tambores. Color plateado escarchado" , img: "bateriaAcustica2.jpg"  },
+    {id:3, nombre: "DTX452K Bateria electrónica", precio: 70538.32, descripcion: "DTX452K Bateria electrónica" , img: "bateriaElectrica1.jpg"  },
+    {id:4, nombre: "VAD103 Bateria electroacústica DESING KIT", precio: 203522.86, descripcion: "VAD103 Bateria electrónica" , img: "bateriaElectrica2.jpg"  },
+    {id:5, nombre: "CCDU141620 Classics custom dual set", precio: 44174.93, descripcion: "Meinl Juegos de platillos" , img: "platillos1.jpg"  },
+    {id:6, nombre: "CC-141620+18 Set de platillos Custom Classic (5)", precio: 43204.41, descripcion: "Meinl Custom Juegos de platillos" , img: "platillos2.jpg"  },
+    {id:7, nombre: "TXR5AW Bolillos Forward 5A RAW", precio: 932.20, descripcion: "Bolillos y Brochas" , img: "accesorios1.png"  },
+    {id:8, nombre: "MDH DRUM Almohadillas amortiguadoras de Miel", descripcion: "Apagadores DRUM HONEY DAMPER PADS" , precio: 710.81, img: "accesorios2.jpg"  },
+] ;
 
-alert("Bienvenido/a "+ usuario + " a AmbiDrums la mejor tienda de instrumentos musicales!")
+//Funciones de búsqueda
+function buscarServicio(arr, filtro) {
+    const encontrado = arr.find((el) => {
+      return el.nombre.includes(filtro);
+    });
+    return encontrado;
+  }
+  function filtrarServicio(arr, filtro) {
+    const filtrado = arr.filter((el) => {
+      return el.nombre.includes(filtro);
+    });
+    return filtrado;
+  }
 
-//crear un usuario para confirmar la compra
-//let contrasenia = prompt("Ingresa una contraseña")
-
-
-//opciones 
-let opcion =prompt("Selecciona un producto: \n\n 1-Baterías acústicas \n 2-Baterías eléctricas \n 3-Platillos \n 4-Acessorios \n 5-Ver el total\n 6-Salir \n");
-
-//declarar las variables inicializandolas en 0
-let suma=0;
-let sumaConComas =0;
-let subtotal=0;
-let subtotalConComas = 0;
-let impuesto =0;
-let impuestoConComas = 0;
-let total =0;
-let conteoDescuento = 0;
-let descuento = 0;
-let descuentoConComas = 0;
-let descuentoFinal = 0;
-let descuentoFinalConComas =0;
 const carrito = [];
-const cantidadProductos = {};
 
-//funciones
+// Agregar productos al carrito
+function btnCarrito(id) {
+    // Buscar el producto en el array de instrumentos
+    const producto = instrumentos.find(item => item.id === id);
 
-//funcion sumar una variable y una constante
-function sumar(num1, num2) {
-    return num1 + num2;
-}
-
-//funcion para instanciar objetos
-function Instrumentos (marca, precio){
-    this.marca = marca;
-    this.precio = precio;
-}
-
-// funcion para recorrer los objetos del array e ir monstrandolos en pantalla
-function Recorrer (object){
-    
-    for (const iterator of object) {
-        alert("Marca: " + iterator.marca + ", Precio: " + iterator.precio);
-    }
-}
-
-// Función para agregar un producto al carrito y actualizar su cantidad
-function agregarProductoACarrito(producto) {
-    if (cantidadProductos[producto.marca]) {
-        cantidadProductos[producto.marca]++;
-    } else {
-        cantidadProductos[producto.marca] = 1;
+    //verificamos y actualizamos al carrito
+    if (producto) {
         carrito.push(producto);
-    }
-}
-
-// Función para mostrar el contenido del carrito
-function mostrarCarrito() {
-    if (carrito.length === 0) {
-        alert("El carrito está vacío");
+        mostrarCarrito();
     } else {
-        let mensaje = "Contenido del carrito:\n\n";
-        carrito.forEach(function(item) {
-            mensaje += "Cantidad: " + cantidadProductos[item.marca] + " Producto: " + item.marca + ", Precio: " + item.precio + "\n\n";
-        });
-        alert(mensaje);
+        console.error("Producto no encontrado");
     }
 }
 
+//Para mostrar los productos del carrito en el html
+function mostrarCarrito() {
+    
+    carritoContenedor.innerHTML = "";
+    totalCarritoElement.textContent = `Total: RD$${totalCarrito().toLocaleString(undefined, { minimumFractionDigits: 2, maximumFractionDigits: 2 })}`;
 
+    carrito.forEach(producto => {
+        const { img, nombre, precio } = producto;
 
-while (opcion != "6"){
+        const html = `
+            <div class="carrito-item">
+                <img src="././assets/img/${img}" alt="${nombre}" />
+                <h4>${nombre}</h4>
+                <p>RD$${precio.toLocaleString(undefined, { minimumFractionDigits: 2, maximumFractionDigits: 2 })}</p>
+            </div>
+        `;
 
-    if(opcion == "1"){
-
-        while(opcion != "0"){
-                opcion = parseFloat( prompt("Las baterías acústicas que tenemos son. Presiona 0 para volver: \n\n 1-Yamaha RDP0F5 HOR Batería acústica Rydeen, Hot Red RD$37,252 \n\n 2-RDP2F5 PB Batería acústica Rydeen, Plateado brillante Bombo 22 RD$39,524.41 \n"));
-                //baterias acusticas
-                if (opcion == "1") {
-                    alert ("Usted seleccionó el producto: Yamaha RDP0F5 HOR Batería acústica Rydeen, Hot Red RD$37,252");
-                    //realizar la suma mediante funcion y poner los valores con la coma y dos decimales
-                    suma = sumar (suma, 37252); 
-                    sumaConComas = suma.toLocaleString(undefined, {minimumFractionDigits: 2, maximumFractionDigits: 2});
-                    //Calcular el impuesto y poner los valores con la coma y dos decimales
-                    impuesto = suma * 0.18;
-                    impuestoConComas = impuesto.toLocaleString(undefined, {minimumFractionDigits: 2, maximumFractionDigits: 2});
-                    //Calcular el subtotal sin impuestos  y poner los valores con la coma y dos decimales
-                    subtotal= suma - impuesto;
-                    subtotalConComas = subtotal.toLocaleString(undefined, {minimumFractionDigits: 2, maximumFractionDigits: 2});
-                    //contador para al final ofrecer descuento al cliente que compre 3 productos
-                    conteoDescuento = conteoDescuento + 1;
-                    const bateriaAcustica1 = new Instrumentos ("Yamaha RDP0F5 HOR Batería acústica Rydeen, Hot Red", "RD$37,252")
-                    agregarProductoACarrito(bateriaAcustica1);
-                    mostrarCarrito()
-
-                } else if(opcion == "2"){
-                    alert ("Usted seleccionó el producto: RDP2F5 PB Batería acústica Rydeen, Plateado brillante Bombo 22 RD$39,524.41");
-                    suma = sumar (suma, 39524.41);
-                    sumaConComas = suma.toLocaleString(undefined, {minimumFractionDigits: 2, maximumFractionDigits: 2});
-                    impuesto = suma * 0.18;
-                    impuestoConComas = impuesto.toLocaleString(undefined, {minimumFractionDigits: 2, maximumFractionDigits: 2});
-                    subtotal= suma - impuesto;
-                    subtotalConComas = subtotal.toLocaleString(undefined, {minimumFractionDigits: 2, maximumFractionDigits: 2});
-                    conteoDescuento = conteoDescuento + 1;
-                    const bateriaAcustica2 = new Instrumentos ("RDP2F5 PB Batería acústica Rydeen, Plateado brillante Bombo 22", "RD$39,524.41")
-                    agregarProductoACarrito(bateriaAcustica2);
-                    mostrarCarrito()
-                    
-                }
-            }
-    }
-
-    //baterias electricas
-    else if(opcion == "2"){
-
-        while(opcion != "0"){
-            opcion = parseFloat( prompt("Las baterias eléctricas que tenemos son. Presiona 0 para volver: \n\n 1-Alesis nitro mesh Kit RD$68,550.00 \n\n 2-VAD103 Bateria electroacústica DESING KIT RD$203,522.86\n"));
-            
-            if (opcion == "1") {
-                alert ("Usted seleccionó el producto: Alesis nitro mesh Kit RD$68,550.00");
-                suma = sumar (suma, 68550.00);
-                sumaConComas = suma.toLocaleString(undefined, {minimumFractionDigits: 2, maximumFractionDigits: 2});
-                impuesto = suma * 0.18;
-                impuestoConComas = impuesto.toLocaleString(undefined, {minimumFractionDigits: 2, maximumFractionDigits: 2});
-                subtotal= suma - impuesto;
-                subtotalConComas = subtotal.toLocaleString(undefined, {minimumFractionDigits: 2, maximumFractionDigits: 2});
-                conteoDescuento = conteoDescuento + 1;
-                const bateriaElectrica1 = new Instrumentos ("Alesis nitro mesh Kit", "RD$68,550.00")
-                agregarProductoACarrito(bateriaElectrica1);
-                mostrarCarrito()
-
-            } else if(opcion == "2"){
-                alert ("Usted seleccionó el producto: VAD103 Bateria electroacústica DESING KIT RD$203,522.86");
-                suma = sumar (suma, 203522.86);
-                sumaConComas = suma.toLocaleString(undefined, {minimumFractionDigits: 2, maximumFractionDigits: 2});
-                impuesto = suma * 0.18;
-                impuestoConComas = impuesto.toLocaleString(undefined, {minimumFractionDigits: 2, maximumFractionDigits: 2});
-                subtotal= suma - impuesto;
-                subtotalConComas = subtotal.toLocaleString(undefined, {minimumFractionDigits: 2, maximumFractionDigits: 2});
-                conteoDescuento = conteoDescuento + 1;
-                const bateriaElectrica2 = new Instrumentos ("VAD103 Bateria electroacústica DESING KIT", "RD$203,522.86")
-                agregarProductoACarrito(bateriaElectrica2);
-                mostrarCarrito()
-            }
-        }
-
-    }
-    //platillos
-    else if(opcion == "3"){
-
-        while(opcion != "0"){
-            opcion = parseFloat( prompt("Los platillos que tenemos son. Presiona 0 para volver: \n\n 1-CC-141620+18 Set de platillos Custom Classic (5) RD$43,204.41 \n\n 2-CC4680-DB CL.CU. Double Bonus SET RD$63,230.30\n"));
-            
-            if (opcion == "1") {
-                alert ("Usted seleccionó el producto: 1-CC-141620+18 Set de platillos Custom Classic (5) RD$43,204.41");
-                suma = sumar (suma, 43204.41);
-                sumaConComas = suma.toLocaleString(undefined, {minimumFractionDigits: 2, maximumFractionDigits: 2});
-                impuesto = suma * 0.18;
-                impuestoConComas = impuesto.toLocaleString(undefined, {minimumFractionDigits: 2, maximumFractionDigits: 2});
-                subtotal= suma - impuesto;
-                subtotalConComas = subtotal.toLocaleString(undefined, {minimumFractionDigits: 2, maximumFractionDigits: 2});
-                conteoDescuento = conteoDescuento + 1;
-                const platillos1 = new Instrumentos ("1-CC-141620+18 Set de platillos Custom Classic (5)", "RD$43,204.41")
-                agregarProductoACarrito(platillos1);
-                mostrarCarrito()
-               
-
-            } else if(opcion == "2"){
-                alert ("Usted seleccionó el producto: CC4680-DB CL.CU. Double Bonus SET RD$63,230.30");
-                suma = sumar (suma, 63230.30);
-                sumaConComas = suma.toLocaleString(undefined, {minimumFractionDigits: 2, maximumFractionDigits: 2});
-                impuesto = suma * 0.18;
-                impuestoConComas = impuesto.toLocaleString(undefined, {minimumFractionDigits: 2, maximumFractionDigits: 2});
-                subtotal= suma - impuesto;
-                subtotalConComas = subtotal.toLocaleString(undefined, {minimumFractionDigits: 2, maximumFractionDigits: 2});
-                conteoDescuento = conteoDescuento + 1;
-                const platillos2 = new Instrumentos ("CC4680-DB CL.CU. Double Bonus SET", "RD$63,230.30")
-                agregarProductoACarrito(platillos2);
-                mostrarCarrito()
-
-            }
-        }
-
-    }
-    //Accesorios
-    else if(opcion == "4"){
-
-        while(opcion != "0"){
-            opcion = parseFloat( prompt("Los accesorios que tenemos son. Presiona 0 para volver: \n\n 1-TXR5AW Bolillos Forward 5A RAW RD$932.20 \n\n 2-TDSB Estuche Deluxe para bolillos color verde militar RD$3,455.04 \n"));
-            
-            if (opcion == "1") {
-                alert ("Usted seleccionó el producto: TXR5AW Bolillos Forward 5A RAW RD$932.20");
-                suma = sumar (suma, 932.20);
-                sumaConComas = suma.toLocaleString(undefined, {minimumFractionDigits: 2, maximumFractionDigits: 2});
-                impuesto = suma * 0.18;
-                impuestoConComas = impuesto.toLocaleString(undefined, {minimumFractionDigits: 2, maximumFractionDigits: 2});
-                subtotal= suma - impuesto;
-                subtotalConComas = subtotal.toLocaleString(undefined, {minimumFractionDigits: 2, maximumFractionDigits: 2});
-                conteoDescuento = conteoDescuento + 1;
-                const accesorios1 = new Instrumentos ("TXR5AW Bolillos Forward 5A RAW", "RD$932.20")
-                agregarProductoACarrito(accesorios1);
-                mostrarCarrito()
-                
-
-            } else if(opcion == "2"){
-                alert ("Usted seleccionó el producto: TDSB Estuche Deluxe para bolillos color verde militar RD$3,455.04");
-                suma = sumar (suma, 3455.04);
-                sumaConComas = suma.toLocaleString(undefined, {minimumFractionDigits: 2, maximumFractionDigits: 2});
-                impuesto = suma * 0.18;
-                impuestoConComas = impuesto.toLocaleString(undefined, {minimumFractionDigits: 2, maximumFractionDigits: 2});
-                subtotal= suma - impuesto;
-                subtotalConComas = subtotal.toLocaleString(undefined, {minimumFractionDigits: 2, maximumFractionDigits: 2});
-                conteoDescuento = conteoDescuento + 1;
-                const accesorios2 = new Instrumentos ("TDSB Estuche Deluxe para bolillos color verde militar", "RD$3,455.04")
-                agregarProductoACarrito(accesorios2);
-                mostrarCarrito()
-            }
-
-        }
-
-    }
-    //Descuento
-    else if (opcion == "5"){
-        
-        if(conteoDescuento >= 3){
-            
-        alert("¡Usted seleccionó: "+conteoDescuento +" Productos!" +"\n\n ¡Aplica para un 10% de descuento!");
-            //calcular el descuento y poner coma al valor y poner los valores con la coma y dos decimales
-            descuento = (10 /100) * suma;
-            descuentoConComas = descuento.toLocaleString(undefined, {minimumFractionDigits: 2, maximumFractionDigits: 2});
-            //calcular el total con el descuento aplicado y poner los valores con la coma y dos decimales 
-            descuentoFinal = suma - descuento;
-            descuentoFinalConComas = descuentoFinal.toLocaleString(undefined, {minimumFractionDigits: 2, maximumFractionDigits: 2});
-            //mostrar todos los resultados
-            mostrarCarrito();
-            alert("Subtotal: RD$"+ subtotalConComas + "\nImpuestos: RD$"+impuestoConComas + "\nTotal con impuestos incl.: RD$"+ sumaConComas + "\nDescuento: RD$" +descuentoConComas + "\nTotal con impuestos y descuento incl.: RD$"+ descuentoFinalConComas);
-
-        }else if(conteoDescuento < 3 && conteoDescuento != 0){
-            //condicion para el que no aplica para el descuento
-            alert("¡Usted seleccionó: "+conteoDescuento +" productos!" +"\n\n¡No aplica para un 10% de descuento!");
-            mostrarCarrito();
-            alert("Subtotal: RD$"+ subtotalConComas + "\nImpuestos: RD$"+impuestoConComas + "\nTotal con impuestos incl.: RD$"+ sumaConComas);
-        }
-        else if(conteoDescuento == 0){
-            alert("Usted no ha seleccionado ningun producto");
-        }
-        
-    }
-    else{
-        alert("opcion no valida");
-      }
-    //condicion de salida
-    opcion =prompt("Selecciona un producto: \n\n 1-Baterías acústicas \n 2-Baterías eléctricas \n 3-Platillos \n 4-Acessorios \n 5-Ver el total\n 6-Salir \n");
+        carritoContenedor.innerHTML += html;
+    });
 }
-//mensaje de despedida
-alert("Gracias" + usuario + " por visitar nuestra tienda espero que vuelvas pronto!");
+
+//Para hacer la sumatoria total
+function totalCarrito() {
+    let total = 0;
+
+    carrito.forEach(producto => {
+        total += producto.precio;
+    });
+
+    return total;
+}
+
+//Para Ver el total en el HTML
+function mostrarTotalCarrito() {
+
+    totalCarritoElement.textContent = `Total: RD$${totalCarrito().toLocaleString(undefined, { minimumFractionDigits: 2, maximumFractionDigits: 2 })}`;
+}
+
+
+// Función para crear estructura html
+function crearHtml(arr) {
+    cardInstrumento.innerHTML = "";
+    //validar qué pasa cuando no recibo ningun array
+    let html;
+    for (const el of arr) {
+      const { img, nombre, precio, id, descripcion } = el;
+  
+      html = `<div  class="card-instrument-tienda" data-aos="fade-right"
+      data-aos-offset="200"
+      data-aos-duration="1500"
+      data-aos-easing="ease-in-sine">
+        <img src="././assets/img/${img}" alt="${nombre}"/>
+          <h3>${nombre}</h3>
+          <p>
+          ${descripcion}
+          </p>
+          <p><strong>RD$${precio.toLocaleString(undefined, {minimumFractionDigits: 2, maximumFractionDigits: 2})}</strong></p>
+          <div class="logo-button-shopp">
+          <button class="btn btn-primary button-instrument-tienda" id="button-instrument-tienda" id="${id}" onclick="btnCarrito(${id})">Agregar al carrito <img src="../assets/img/shopping.svg" alt="logo-shopping"></button>
+          </div>
+      </div>`;
+      //se la agrego al contenedor
+      cardInstrumento.innerHTML += html;
+    }
+  }
+  crearHtml(instrumentos)
+
+  
 
 
